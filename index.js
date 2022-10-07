faad86a24972ba14ede9644c9cf73aab04dee053
K1 <k1.codingworld@gmail.com>
2022-10-06 22:36:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,22 +10,6 @@ extended: true
 
 app.get("/", (req, res) => {
 	res.send(`<body><iframe src='https://darkguybot.github.io/docs'></iframe></body>`)
-})
-
-app.get("/welcome", async (req, res) => {
-	let sp = req.query;
-console.log(sp)
-let lienzo = await new wel()
-	.setGuildName(sp.guild)
-  .setAvatar(sp.avatar)
-  .setBackground(sp.background)
-  .setUsername(sp.username)
-	.setDiscriminator(sp.discriminator)
-	.toAttachment();
-// https://MyApiForDG.k1a.repl.co?guild=DarkGuy&avatar=https://cdn.discordapp.com/avatars/838091364344397835/73a8a7867455ecc991b810a41f37c9a8.jpg&background=https://media.discordapp.net/attachments/921798413547352065/973120096664973332/442432.jpg&username=K1&discriminator=1960		
-	res.json({
-		img: Buffer.from(lienzo.toBuffer(), 'utf8')
-	})
 });
 
 app.get('/niby-koya', async (req, res) => {
