10b2659d1d39a1a761b692d8a26655903f0ab924
K1 <k1.codingworld@gmail.com>
2022-10-06 22:51:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const app = express();
-const nw = require('niby-welcomes');
-const Canvas = require('canvas');
+const express  = require('express');
+const nw       = require('niby-welcomes');
 const {Buffer} = require('node:buffer');
-const wel = require('./canvasEngine/Welcome');
+
+const app = express();
+
 app.use(express.urlencoded({
 extended: true
 }));
 
 app.get("/", (req, res) => {
-	res.send(`<body><iframe src='https://darkguybot.github.io/docs'></iframe></body>`)
+	res.send(`<body><iframe src='https://darkguybot.github.io/docs'></iframe></body>`);
 });
 
 app.get('/niby-koya', async (req, res) => {
 	try {
-	let r = req.query;
+  	let query = req.query;
+    
+  	let img = await new nw.Welcome()
+  	.setUsername(query.username || 'User', { color: "#ffffff" })
+	  .setWelcomeMessage(query.message)
+  	.setAvatar(query.avatar)
+  	.setBackgroundUrl(query.background)
+  	.setBorder(query.border === "true" ? true : false)
+    .setStyle('koya')
+  	.setMemberCount(`Eres el miembro #${query.count || 0}`);
 
-	let img = await new nw.Welcome()
-	.setUsername(r.username, { color: "#ffffff" })
-	.setWelcomeMessage(r.message)
-	.setAvatar(r.avatar)
-	.setBackgroundUrl(r.background)
-	.setBorder(r.border === "true" ? true : false)
-  .setStyle('koya')
-	.setMemberCount(`Eres el miembro #${r.count}`);
-
-	//console.log(await img.build());
-	res.json(await img.build());
+  	res.json(await img.build());
 	} catch (_) { 
 		console.log(_);
 	}
 });
 	
-app.listen(0202, () => console.log('Online'));
+app.listen(process.env.PORT || 3434, () => console.log('Server on port', process.env.PORT || 3434));
